9e089276847f2826d585aa678f2254aa2e03d86e
April-Zhangjq <160635561+April-Zhangjq@users.noreply.github.com>
2026-03-18 18:32:08 +0800

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python main.py score --model_path ./core/model/model.pt --protein_pdb ./examples
 python main.py score --model_path ./core/model/model.pt --protein_pdb ./examples/1h22/1h22_protein.pdb --ligand_mol2 ./examples/1h22/1h22_decoys.mol2 --output_dir ./test_output/1h22
 
 # optimize
-python main.py optimize --model_path ./core/model/model.pt --protein_pdb ./examples/1h22/1h22_protein.pdb --ligand_mol2 ./examples/1h22/1h22_1.mol2 --output_dir ./test_output/1h22_1 --max_cycles 10 --convergence_threshold 0.001
+python main.py optimize --model_path ./core/model/model.pt --protein_pdb ./examples/1h22/1h22_protein.pdb --ligand_mol2 ./examples/1h22/1h22_1.mol2 --output_dir ./test_output/1h22_1 --max_cycles 10 --early_stop_threshold 0.001
